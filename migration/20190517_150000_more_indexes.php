0e0a852dedf87bf70e1f5a24e87a4d4f49d3c8cf
Christophe Gosiau <christophe@tigron.be>
2019-05-17 15:12:27 +0200

    add extra index to improve transaction search speed

---
+++ migration/20190517_150000_more_indexes.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Database migration class
+ *
+ * @author Gerry Demaret <gerry@tigron.be>
+ */
+namespace Skeleton\Transaction;
+
+use \Skeleton\Database\Database;
+
+class Migration_20190517_150000 extends \Skeleton\Database\Migration {
+
+	/**
+	 * Migrate up
+	 *
+	 * @access public
+	 */
+	public function up() {
+		$db = Database::get();
+
+		$db->query("
+			ALTER TABLE `transaction`
+			ADD INDEX `completed_failed_frozen_locked` (`completed`, `failed`, `frozen`, `locked`);"
+		);
+
+	}
+
+	/**
+	 * Migrate down
+	 *
+	 * @access public
+	 */
+	public function down() {
+
+	}
+}
